122c8e165072eb209ca91f51a5c0575f89788134
Felipe Cárdenas <felipecardenas301@gmail.com>
2023-05-09 19:22:17 -0400

    [update] Add web

--- index.ts
+++ index.ts
@@ -1,9 +1,25 @@
 import Climate from './climate/climate.ts'
 import AgentMqtt from './mqtt/agent.ts'
+import { Application } from 'https://deno.land/x/oak/mod.ts'
+
+async function startWeb() {
+	const app = new Application()
+	app.use((ctx) => {
+		ctx.response.body = {
+			health: true,
+		}
+	})
+
+	await app.listen({
+		port: 8000,
+	})
+}
 
 function main() {
 	// Climate
 	const climate = new Climate()
+	// Init API
+	startWeb()
 	// Agent
 	const fakeAgent = new AgentMqtt(climate)
 	setInterval(() => {
